1a32ba808450274ade514b73f12315f43e4e219a
glynda <glyncan123@gmail.com>
2023-03-07 20:01:11 -0800

    first commit

---
+++ readme.js
@@ -0,0 +1,15 @@
+
+<html> 
+<body>
+<h2>Welcome to Javascript</h2>
+<p id = "name">Wat's your name</p>
+<p id = "greeting">Hello there</p>
+<button type="button" onclick='document.getElementById("name").innerHTML= "Excellent"'>Just Click me</button>
+<button type="button" onclick='document.getElementById("greeting").innerHTML = "Hello JavaScript!"'>Click Me!</button>
+
+</body>
+</html>
+
+
+
+
